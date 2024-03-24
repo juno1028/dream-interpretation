78a15065e2e8d56437e62d1a43916a666e7e3b72
juno1028 <juno9325@gmail.com>
2024-03-24 16:03:56 +0900

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <button className="start-button">시작하기</button>
+    </div>
+  );
+}
+
+export default HomePage;
